fd67e0f839d0e717f601c01ca0aed868f12540ee
yoann189.labbe@laposte.net <y.labbe.contact@gmail.comgit log -pwild-gitls$ lspwd~cdgit config --global user.email y.labbe.contact@gmail.com>
2021-08-18 08:17:59 +0200

    fin

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require ("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin:"http://localhost:3001"}));
+app.use(cors({ credentials: true, origin:["http://localhost:3000", "http://localhost:3001"]}));
 require("./routes")(app);
 
 const port = 3031;
